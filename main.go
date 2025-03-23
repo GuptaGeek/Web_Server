3456814c37e9cdf949616b543b37fdd091f79a88
GuptaGeek <sheetalguptamrt121@gmail.com>
2025-03-23 16:05:45 +0530

    add some functions to the main.go

--- main.go
+++ main.go
@@ -4,6 +4,19 @@ import (
 	"fmt"
 	"net/http"
 )
+func  Home(w http.ResponseWriter,r * http.Request){
+	fmt.Fprintf(w,"This is the Home Page")
+
+}
+func About(w http.ResponseWriter,r *http.Request){
+	sum,_:=AddValues(2,2)
+	fmt.Fprintf(w,fmt.Sprintf("This is at the about page and 2+2 %d",sum))
+}
+func AddValues(x, y int)(int,error){
+
+	
+   return x+y,nil
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
