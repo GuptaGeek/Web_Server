ea33e9ce2bb4a6d11ac7b60f9ea2d883a38b417f
GuptaGeek <sheetalguptamrt121@gmail.com>
2025-03-23 23:41:09 +0530

    render.go

--- main.go
+++ main.go
@@ -3,28 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 )
 
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
 const portNumber = ":8080"
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing templates:", err)
-		return
-	}
-}
 
 // main is the main application function
 func main() {
