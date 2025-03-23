0af7f32c9e3470fb005edc6579deceb16e88c03a
GuptaGeek <sheetalguptamrt121@gmail.com>
2025-03-23 23:22:28 +0530

    add randerTemplate function

--- main.go
+++ main.go
@@ -8,17 +8,22 @@ import (
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-
+	renderTemplate(w, "home.page.tmpl")
 }
 
 const portNumber = ":8080"
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-
+	renderTemplate(w, "about.page.tmpl")
 }
-func renderTemplate(w http.ResponseseWriter, r *http.Request){
-	parsedTemplate,_:=template.ParseFiles()
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing templates:", err)
+		return
+	}
 }
 
 // main is the main application function
