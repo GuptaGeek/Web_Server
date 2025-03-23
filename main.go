7d9b85c0ef069f5ef091907e12042a3a14f19206
GuptaGeek <sheetalguptamrt121@gmail.com>
2025-03-23 16:22:05 +0530

    add handler functions like about, home and addValues

--- main.go
+++ main.go
@@ -4,28 +4,31 @@ import (
 	"fmt"
 	"net/http"
 )
-func  Home(w http.ResponseWriter,r * http.Request){
-	fmt.Fprintf(w,"This is the Home Page")
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the Home Page")
 
 }
-func About(w http.ResponseWriter,r *http.Request){
-	sum,_:=AddValues(2,2)
-	fmt.Fprintf(w,fmt.Sprintf("This is at the about page and 2+2 %d",sum))
-}
-func AddValues(x, y int)(int,error){
 
-	
-   return x+y,nil
+const portNumber = ":8080"
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum, _ := addValues(2, 2)
+	fmt.Fprintf(w, fmt.Sprintf("This is at the about page and 2+2 %d", sum))
 }
 
+// addValues adds two integers and return the sum
+func addValues(x, y int) (int, error) {
+	return x + y, nil
+}
+
+// main is the main application function
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello World!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of Bytes Written: %d", n))
 
-	})
-	_ = http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
